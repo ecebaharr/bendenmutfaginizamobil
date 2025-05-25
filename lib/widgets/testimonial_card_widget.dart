7c6e405e5ea1f57996a7686fd7c6d8cdbf2626c0
ecebaharr <ecebahaarr@gmail.com>
2025-05-25 20:26:33 +0300

    mobil

--- lib/widgets/testimonial_card_widget.dart
+++ lib/widgets/testimonial_card_widget.dart
@@ -31,7 +31,7 @@ class TestimonialCardWidget extends StatelessWidget {
             ),
             const SizedBox(width: 12),
 
-            // 📝 İçerik
+            // 📝 İçeri
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
